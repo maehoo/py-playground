b6dbb9cee6db464802f59b78f14908e098414a82
MaeHoo <maehoo321@gmail.com>
2025-05-13 11:46:51 +0900

    문자열 자료형 -03

---
+++ py-stduy/algorithm/a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    int N.M;
+    scanf("%d%d",&N,&M);
+    char * arr1[N];
+    char * arr2[M];
+    char str[500];
+    int i;
+    for(i=0;i<N;i++)
+    {
+        scanf("%s",str);
+        arr[i] =  
+        
+    }
+    for(i=0;i<M;i++)
+    {
+        scanf("%s",&str[i])
+    }
+}
