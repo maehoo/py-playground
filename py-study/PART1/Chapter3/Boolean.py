85f036af85c430e3706dd5ed066021495c88cae1
MaeHoo <maehoo321@gmail.com>
2025-08-17 11:42:57 +0900

    불리언

---
+++ py-study/PART1/Chapter3/Boolean.py
@@ -0,0 +1,14 @@
+# 불리언
+# 불리언데이터 타입의 유일한 값은 True(참)와 False(거짓)다.
+
+#True
+print(bool(True))
+print(bool(2))
+print(bool(45))
+print(bool(-45))
+
+#False
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+#0인 값은 False로 간주한다.
