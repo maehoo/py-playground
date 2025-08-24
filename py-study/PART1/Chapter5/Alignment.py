934250ef850473a96e113bee38572040d64c1e83
MaeHoo <maehoo321@gmail.com>
2025-08-25 06:04:00 +0900

    문자열 정렬

---
+++ py-study/PART1/Chapter5/Alignment.py
@@ -0,0 +1,14 @@
+#정렬
+sentence = "a duck goes into a bar"
+
+#가운데 정렬
+print(sentence.center(50))
+""">>>               a duck goes into a bar               """
+
+#왼쪽 정렬
+print(sentence.ljust(50))
+""">>>a duck goes into a bar                              """
+
+#오른쪽 정렬
+print(sentence.rjust(50))
+""">>>                              a duck goes into a bar"""
